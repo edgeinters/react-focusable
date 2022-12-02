2a907a34050d3f180036851543ff795cf59dc77d
Ladislav Navratil <navratill@gmail.com>
2022-12-02 09:55:31 +0100

    debugging of vertical focus

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
-import { FixedList } from './components/FixedList'
+import { Focusable } from 'react-focusable'
 import { Menu } from './components/Menu'
 
 const App = () => {
     return (
         <>
             <Menu />
-            <FixedList focusableKey='first' />
+            {/* <FocusableContainer
+                focusableBounds={{
+                    x: 0, y: 300, width: 300, height: 100
+                }}
+                focusableKey='container'
+            > */}
+            <Focusable
+                focusableBounds={{ x: 0, y: 0, width: 100, height: 100 }}
+                focusableKey="button"
+            >
+                {(focusable) => {
+
+                    return <p style={{ color: focusable.isFocused ? 'red' : 'black' }}>ahoj</p>
+                }}
+
+            </Focusable>
+            {/* </FocusableContainer> */}
+            {/* <FixedList focusableKey='first' />
             <FixedList focusableKey='second' />
-            <FixedList focusableKey='third' />
+            <FixedList focusableKey='third' /> */}
         </>
     )
 }
