54c5e8b4d7cb4a964ff0da87c15f11129094d903
Ladislav Navratil <navratill@gmail.com>
2022-12-12 14:03:22 +0100

    Updates

---
+++ precache-manifest.9a92a87c4a7124f68aa0db0f2453b2a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1130b4ef846555da87a47f56cf962d57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07b4fd03532e42001a5c",
+    "url": "./static/js/2.0fd8ee4c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0fd8ee4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0586b5535246f4f0597",
+    "url": "./static/js/main.37fb4563.chunk.js"
+  },
+  {
+    "revision": "8684c080bf92f16cc227",
+    "url": "./static/js/runtime-main.def36b62.js"
+  }
+]);
